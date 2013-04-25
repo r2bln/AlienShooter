306928c70cd449f1a0c0b80148424c1b147a7fc0
Ivan <kulikovis91@gmail.com>
2013-04-25 22:07:21 +0400

    Some weird methods and resources added..

--- AlienShooter/Level.cs
+++ AlienShooter/Level.cs
@@ -23,5 +23,24 @@ namespace AlienShooter
                 }
             }
         }
+        public void DrawHex(Graphics canvas)
+        {
+            for (int i = 0; i < height - 128; i += 55)
+            {
+                for (int j = 0; j < width - 128 - 64; j += 128+64)
+                {
+                    if (i%2 == 0)
+                    {
+                        canvas.DrawImage(Resource1.hex, j + 95, i);
+                        canvas.DrawRectangle(new Pen(Color.Black), j + 95, i, 128, 128);
+                    }
+                    else
+                    {
+                        canvas.DrawImage(Resource1.hex, j, i);
+                        canvas.DrawRectangle(new Pen(Color.Black), j, i, 128, 128);
+                    }
+                }
+            } 
+        }
     }
 }
